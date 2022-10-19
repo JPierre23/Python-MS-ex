d1005f500b9f6b71ec09189419a61edac4f54fda
Jonathan Pierre <JonWPierre@gmail.com>
2022-10-18 22:05:32 -0400

    fast api router code

--- Makefile
+++ Makefile
@@ -2,8 +2,8 @@ install:
 	#install commands
 		pip install --upgrade pip &&\
 		pip install -r requirements.txt
-		
 
+		
 format:
 	#format code
 	black *.py mylib/*.py
--- main.py
+++ main.py
@@ -1,3 +1,38 @@
 from mylib.logic import wiki
 from fastapi import FastAPI
+from mylib.logic import search_wiki
+import uvicorn
+
 # print(wiki())
+
+@app.get("/")
+async def root():
+    return {"message": "Wikipedia API.  Call /search or /wiki"}
+
+
+@app.get("/search/{value}")
+async def search(value: str):
+    """Page to search in wikipedia"""
+
+    result = search_wiki(value)
+    return {"result": result}
+
+
+@app.get("/wiki/{name}")
+async def wiki(name: str):
+    """Retrieve wikipedia page"""
+
+    result = wikilogic(name)
+    return {"result": result}
+
+
+@app.get("/phrase/{name}")
+async def phrase(name: str):
+    """Retrieve wikipedia page and return phrases"""
+
+    result = wikiphrases(name)
+    return {"result": result}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, port=8080, host="0.0.0.0")
